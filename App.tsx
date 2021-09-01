20c3dea4a9dee53b62e21ef87cbe40060aaeb250
Michael Villarruel <alejovillarruel@gmail.com>
2021-09-01 18:01:09 -0500

    basic design - HomeScreen

--- App.tsx
+++ App.tsx
@@ -1,10 +1,16 @@
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
+import { StatusBar } from 'react-native';
 import { Navigator } from './src/navigator/Navigator';
 
 const App = () => {
   return (
     <NavigationContainer>
+      <StatusBar
+        backgroundColor='transparent'
+        translucent={true}
+        barStyle='dark-content'
+      />
       <Navigator />
     </NavigationContainer>
   )
