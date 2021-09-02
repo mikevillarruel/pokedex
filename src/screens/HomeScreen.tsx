4e0c3b3930268f4cc37efad0c2b5620506fa8188
Michael Villarruel <alejovillarruel@gmail.com>
2021-09-01 19:02:42 -0500

    pokemons mapping

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { Image, Text, View } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { styles } from '../theme/appTheme';
+import { usePokemonPaginated } from '../hooks/usePokemonPaginated';
 
 export const HomeScreen = () => {
 
     const { top } = useSafeAreaInsets();
+    const { simplePokemonList } = usePokemonPaginated();
 
     return (
         <>
